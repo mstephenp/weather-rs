3c267aea655129a34bcf6f914e86d0b1e0ebc92d
Michael <mstephenp@protonmail.com>
2020-12-23 05:30:01 -0600

    working

--- weather-app/src/main.rs
+++ weather-app/src/main.rs
@@ -1,5 +1,23 @@
 
 
-fn main() {
-    println!("working!")
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+#[allow(non_snake_case)]
+struct TodoItem {
+    userId: i64,
+    id: i64,
+    title: String,
+    completed: bool
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://jsonplaceholder.typicode.com/todos/1")
+        .await?
+        .json::<TodoItem>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
